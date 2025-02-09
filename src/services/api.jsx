844af0dca2962b97b8d7dd9694d2f01e90a95a1f
aminayusufova <yusufovamina25@gmail.com>
2025-02-10 03:00:03 +0400

    upd

--- src/services/api.jsx
+++ src/services/api.jsx
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const api = axios.create({
-  baseURL: "http://localhost:5155",
+  baseURL: "https://respective-anastassia-wishlistwebapp-7a7676c1.koyeb.app",
 });
 
 api.interceptors.request.use((config) => {
